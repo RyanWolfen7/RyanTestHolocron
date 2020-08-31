3a2dbf64ca9a3a363844cffb38727e6b5f298d1b
RyanWolfen7 <wolfen7lada@gmail.com>
2020-08-31 16:09:25 -0500

    getting closer i think

--- src/components/RyanTest.jsx
+++ src/components/RyanTest.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { loadLanguagePack, updateLocale } from '@americanexpress/one-app-ducks';
 import { connect } from 'react-redux';
 import { fromJS } from 'immutable';
@@ -8,7 +8,8 @@ import { GET_SUMMARY } from '../types';
 
 export const RyanTest = props => {
   console.log('props\n', props)
-
+  // const { isLoading, marketSummary, error } = useSelector(( state ) => { state.getSummary})
+  
   return (
     <div>
       <h1>Booyah</h1>
@@ -28,6 +29,9 @@ export const mapDispatchToProps = (dispatch) => ({
     await dispatch(updateLocale(target.value));
     await dispatch(loadLanguagePack('ryan-test', { fallbackLocale: 'en-US' }));
   },
+  markets: async ( type ) => {
+    await dispatch(yahooMarket.getSummary())
+  }
 });
 
 export const mapStateToProps = (state) => {
@@ -36,30 +40,33 @@ export const mapStateToProps = (state) => {
     ['intl', 'languagePacks', localeName, 'ryan-test'],
     fromJS({})
   ).toJS();
+  console.log('state', state)
+  const yahooMarket = state.getIn(['intl', 'markets'])
+  console.log('state2', yahooMarket)
 
   return {
     languageData: languagePack && languagePack.data ? languagePack.data : {},
-    localeName,
+    localeName
   };
 };
 
-const loadModuleData = async ({ store, fetchClient , ownProps, module }) => {
-  console.log('store\n', store)
-  console.log('fetchClient\n', fetchClient)
-  console.log('ownProps\n', ownProps)
-  const moduleState = store.getState().getIn(['modules', 'ryan-test'])
-  if (moduleState.get('isComplete') && moduleState.get('data')) {
-    return;
-  }
-  store.dispatch({ type: GET_SUMMARY })
-  const response = await fetchClient(yahooMarket.getSummary())
-  const data = await response.json()
-  store.dispatch({ type: GET_SUMMARY, data})
-  console.log('module\n', module, moduleState)
-  // // If isComplete and data already exists dont run request again
-}
+// export const loadModuleData = async ({ store, fetchClient , ownProps, module }) => {
+//   console.log('store\n', store)
+//   console.log('fetchClient\n', fetchClient)
+//   console.log('ownProps\n', ownProps)
+//   const moduleState = store.getState().getIn(['modules', 'ryan-test'])
+//   if (moduleState.get('isComplete') && moduleState.get('data')) {
+//     return;
+//   }
+//   store.dispatch({ type: GET_SUMMARY })
+//   const response = await fetchClient(yahooMarket.getSummary())
+//   const data = await response.json()
+//   store.dispatch({ type: GET_SUMMARY, data})
+//   console.log('module\n', module, moduleState)
+//   // // If isComplete and data already exists dont run request again
+// }
 
-// export const loadModuleData = ({ store: { dispatch } }) => { dispatch(loadLanguagePack('ryan-test', { fallbackLocale: 'en-US' }))}
+export const loadModuleData = ({ store: { dispatch } }) => { dispatch(loadLanguagePack('ryan-test', { fallbackLocale: 'en-US' }))}
 
 RyanTest.holocron = {
   name: 'ryan-test',
